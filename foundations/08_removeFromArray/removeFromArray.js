22ebff1dcd07da0cd7d2d776d00cb7a61a5a62b8
Andre5142 <243345333+Andre5142@users.noreply.github.com>
2026-04-02 10:36:55 +0200

    Finished course material
    Tested my knowledge on what I learned so far in javascript
    Done exercises span from 05_joinStrings including up to 11_tempConversion

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function (arr, ...args) {
+    const remArray = []
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+            remArray.push(item)
+        }
+    })
+    return remArray
+}
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (fahrenheit) {
+  //(x − 32) × ⁠5/9°C
+  let celsius = (fahrenheit - 32) * 5 / 9 * 10
+  return Math.round(celsius) / 10
+}
 
-const convertToFahrenheit = function() {
-};
-
+const convertToFahrenheit = function (celsius) {
+  //(x × ⁠9/5+32)°F
+  let fahrenheit = (celsius * 9 / 5 + 32) * 10
+  return Math.round(fahrenheit) / 10
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
